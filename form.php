70fe02da7dedac2328db00c96e9b63a7cd961422
onebadger <yakov.barsukov@gmail.com>
2019-05-29 00:43:51 +0300

    files update

--- form.php
+++ form.php
@@ -1,39 +1,30 @@
 <?php 
-
-if(isset($_POST['email'])) {
-  $message = 'Mail: ' .$_POST['email'];
-  mail('yakov.barsukov@gmail.com', 'barsukov.ru', $message);
+if(isset($_POST['user_email']) && empty($_POST['user_name'])) {
+  $message = 'Name: ' .$_POST['user_name'] . "\n"
+    .'Email: ' .$_POST['user_email'] . "\n";
+  mail('yakov.barsukov@gmail.com', 'barsukova.ru call back form', $message);
 } else {
-  header('Refresh: 3; URL=../index.html');
+  header('Refresh: 3; URL=index.html');
   exit(0);
 }
 ?>
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="ru">
-  <head>
-   
-    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0" />
-    <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <meta name="robots" content="noindex,nofollow" />
-    <title>Barsukova</title>
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700,900&amp;subset=cyrillic" rel="stylesheet" />
-    <meta http-equiv="refresh" content="15;url=index.html" />
-    <link rel="stylesheet" type="text/css" href="css/normalize.css" />
-    
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-    
-    <link rel="stylesheet" type="text/css" href="main.css" />
-<!--    <link rel="stylesheet" type="text/css" href="css/responsive.css" />-->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  </head>
-  <body>
-    <div id="hohoho">
-      <p class="text-white" for="exampleInputEmail1">спасибо пожалуйста!</p>
-      <form action="http://barsukov.ru/"><button type="submit" class="btn btn-success">back</button></form>
-    </div>
-  </body>
-</html>
+<head>
+  <meta charset="utf-8">
+  <title>barsukova</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700,900&amp;subset=cyrillic" rel="stylesheet" />
+</head><body>
+ <style>
+    body{background-color:#E4678D;display:flex;flex-direction:column;justify-content:center;align-items:center;align-content:center;}#hohoho{text-align:center;width:280px;position:absolute;top:calc(35%);left:calc(50% - 140px);}form input{margin-bottom:12px;}a{color:white;text-decoration:none!important;}a:visited{color:white;}
+  </style>
+  <div id="hohoho">
+    <p class="text-white">спасибо пожалуйста!</p>
+    <button class="btn btn-secondary"><a href="http://barsukov.ru/">back</a></button>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body></html>
